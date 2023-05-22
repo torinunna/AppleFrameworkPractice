21f5e118af343c8d6da586d6962ea9cf46f7f3fc
torinunna <torinunna9588@gmail.com>
2023-05-22 16:51:56 +0900

    add components to DetailVC

--- AppleFrameworkPractice/FrameworkDetailViewController.swift
+++ AppleFrameworkPractice/FrameworkDetailViewController.swift
@@ -9,6 +9,10 @@ import UIKit
 
 class FrameworkDetailViewController: UIViewController {
 
+    @IBOutlet weak var thumbnailImage: UIImageView!
+    @IBOutlet weak var titleLabel: UILabel!
+    @IBOutlet weak var descriptionLabel: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
