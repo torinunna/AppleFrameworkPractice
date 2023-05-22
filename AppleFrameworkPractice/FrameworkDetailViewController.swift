e836ee1136c447ef96a9451ca7309716a453ba69
torinunna <torinunna9588@gmail.com>
2023-05-22 16:34:54 +0900

    create Detail.storyboard and FramewrokDetailVC

---
+++ AppleFrameworkPractice/FrameworkDetailViewController.swift
@@ -0,0 +1,18 @@
+//
+//  FrameworkDetailViewController.swift
+//  AppleFrameworkPractice
+//
+//  Created by YUJIN KWON on 2023/05/22.
+//
+
+import UIKit
+
+class FrameworkDetailViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+ 
+
+}
