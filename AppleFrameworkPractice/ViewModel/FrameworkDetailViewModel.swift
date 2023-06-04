817efa6e9f04b0d367d862f60efebce3c51833dc
torinunna <torinunna9588@gmail.com>
2023-06-04 23:29:26 +0900

    organise and create files to refactor into MVVM

---
+++ AppleFrameworkPractice/ViewModel/FrameworkDetailViewModel.swift
@@ -0,0 +1,8 @@
+//
+//  FrameworkDetailViewModel.swift
+//  AppleFrameworkPractice
+//
+//  Created by YUJIN KWON on 2023/06/04.
+//
+
+import Foundation
---
+++ AppleFrameworkPractice/ViewModel/FrameworkListViewModel.swift
@@ -0,0 +1,8 @@
+//
+//  FrameworkListViewModel.swift
+//  AppleFrameworkPractice
+//
+//  Created by YUJIN KWON on 2023/06/04.
+//
+
+import Foundation
